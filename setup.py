399db82f0d01a935a20302137a9178b6a44c01a5
indigohedgehog <indigohedgehog@gmail.com>
2021-05-01 23:33:57 -0500

    Update version for release

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name='uapy',
-    version='1.0.1',
+    version='1.0.2',
     author="Andres Castillo",
     author_email="indigohedgehog@gmail.com",
     description='Python wrapper for Linux UAPI ioctl',
